6c10811bfa072f98757e5d07ba1a5fd860760ce3
Денис Андрейчук <denis.andrei4uk@yandex.ua>
2019-10-29 19:33:55 +0200

    Added new target for tests.

---
+++ EasySwiftLayoutTests/EasySwiftLayoutTests.swift
@@ -0,0 +1,34 @@
+//
+//  EasySwiftLayoutTests.swift
+//  EasySwiftLayoutTests
+//
+//  Created by Денис Андрейчук on 10/7/19.
+//  Copyright © 2019 Denis Andreychuk. All rights reserved.
+//
+
+import XCTest
+import EasySwiftLayout
+
+class EasySwiftLayoutTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
